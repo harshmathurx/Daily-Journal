294e2ac85a976a0e80d0e8aff51b21065d150fcc
harshmathurx <harshmathurx@gmail.com>
2021-07-23 12:53:32 +0530

    Added procfile

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.get("/posts/:name",function(req,res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function() {
+  console.log("Server started successfully");
 });
